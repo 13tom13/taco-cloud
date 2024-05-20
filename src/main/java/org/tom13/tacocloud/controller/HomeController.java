abedbaecd258c73570b93c0b87e1b2044bc212e6
13tom13 <13tom13@mail.ru>
2024-05-20 19:56:20 +0200

    init commit

---
+++ src/main/java/org/tom13/tacocloud/controller/HomeController.java
@@ -0,0 +1,13 @@
+package org.tom13.tacocloud.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+}
---
+++ src/test/java/org/tom13/tacocloud/TacoCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tom13.tacocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

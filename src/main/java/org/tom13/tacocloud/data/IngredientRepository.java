c0321b2aacf454902ed6c2682cb43142db1c255a
13tom13 <13tom13@mail.ru>
2024-05-26 23:26:08 +0200

    CrudRepository support implemented

--- src/main/java/org/tom13/tacocloud/data/IngredientRepository.java
+++ src/main/java/org/tom13/tacocloud/data/IngredientRepository.java
@@ -1,9 +1,11 @@
 package org.tom13.tacocloud.data;
 
+import org.springframework.data.repository.CrudRepository;
 import org.tom13.tacocloud.entity.model.Ingredient;
+
 import java.util.Optional;
 
-public interface IngredientRepository {
+public interface IngredientRepository extends CrudRepository<Ingredient, String> {
 
     Iterable<Ingredient> findAll();
 
--- src/main/java/org/tom13/tacocloud/data/OrderRepository.java
+++ src/main/java/org/tom13/tacocloud/data/OrderRepository.java
@@ -1,7 +1,8 @@
 package org.tom13.tacocloud.data;
 
+import org.springframework.data.repository.CrudRepository;
 import org.tom13.tacocloud.entity.model.TacoOrder;
 
-public interface OrderRepository {
+public interface OrderRepository extends CrudRepository<TacoOrder, Long> {
     TacoOrder save(TacoOrder order);
 }
